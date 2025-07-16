1e4b73bd3833558de7e7307af2fe2a68c4443cbe
sunny199906 <73141903+sunny199906@users.noreply.github.com>
2025-07-17 02:23:55 +0800

    chart of full year

--- src/utils/chart-data-analyzer.ts
+++ src/utils/chart-data-analyzer.ts
@@ -14,6 +14,25 @@ export function createMonthlyTimeData(chartData: chartData) : DataPropsType {
             due: [],
         }
     };
+    const currentDate = new Date();
+    let month = currentDate.getMonth() + 1; // Months are 0-indexed in JavaScript
+    let year = currentDate.getFullYear() - 1;//start from last year
+    // let isCurrentYear = false;
+    
+    for (let i = 0; i < 13; i++) {
+        if(month < 13){
+            processedData.data.received.push({ x: month + '/' + year, y: 0 });
+            processedData.data.due.push({ x: month + '/' + year, y: 0 });
+            month++;
+        } else {
+            month = 1; // Reset to January
+            year++;
+            // processedData.data.received.push({ x: month + '/' + year, y: 0 });
+        }
+    }
+    console.log("Processed Data Received: ", processedData.data.received);
+    console.log("Processed Data Due: ", processedData.data.due);
+
     chartData.invoiceChequeChartData.forEach((item : invoiceChequeChartData) => {
         // const shortMonth = item.paymentDate.toLocaleString('default', { month: 'short'});
         const paymentDate = new Date(item.paymentDate);
@@ -24,11 +43,12 @@ export function createMonthlyTimeData(chartData: chartData) : DataPropsType {
         if(foundMonthReceivedData != null){
             console.log("Found month in received: ", shortMonthAndYear, item.amount);
             foundMonthReceivedData.y += item.amount;
-        } else {
-            console.log("Adding new month to received: ", shortMonthAndYear, item.amount);
-            processedData.data.received.push({ x: shortMonthAndYear, y: item.amount });
-            console.log("Processed Data Received: ", processedData.data.received);
-        }
+        } 
+        // else {
+        //     console.log("Adding new month to received: ", shortMonthAndYear, item.amount);
+        //     processedData.data.received.push({ x: shortMonthAndYear, y: item.amount });
+        //     console.log("Processed Data Received: ", processedData.data.received);
+        // }
     });
 
     chartData.invoiceChartData.forEach((item : invoiceChartData) => {
@@ -40,9 +60,10 @@ export function createMonthlyTimeData(chartData: chartData) : DataPropsType {
 
         if(foundMonthDueData != null){
             foundMonthDueData.y += (item.amount - item.paidAmount);
-        } else if((item.amount - item.paidAmount) > 0) {
-            processedData.data.due.push({ x: shortMonthAndYear, y: item.amount - item.paidAmount });
-        }
+        } 
+        // else if((item.amount - item.paidAmount) > 0) {
+        //     processedData.data.due.push({ x: shortMonthAndYear, y: item.amount - item.paidAmount });
+        // }
     });
 
     // if(processedData.data.received.length < processedData.data.due.length){
